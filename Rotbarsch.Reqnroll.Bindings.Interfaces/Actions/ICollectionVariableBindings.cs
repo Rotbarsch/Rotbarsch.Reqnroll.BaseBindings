1de4cb9fd01aaba0ebef01c3f4e263d25bf3e93d
Rotbarsch <denied@denied.com>
2026-07-27 11:02:24 +0200

    Removed remnants of Bindings.Interfaces

--- Rotbarsch.Reqnroll.Bindings.Interfaces/Actions/ICollectionVariableBindings.cs
+++
@@ -1,9 +0,0 @@
-namespace Rotbarsch.Reqnroll.Bindings.Interfaces.Actions;
-
-/// <summary>
-/// Step bindings for actions on collection variables (e.g., lists of items).
-/// </summary>
-public interface ICollectionVariableBindings
-{
-    void StoreCollectionLengthInVariable(string collectionVariableName, string targetVariableName);
-}
